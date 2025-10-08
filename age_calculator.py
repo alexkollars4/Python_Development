a46bc26646fffe45c6572ea168dd16f735814102
Alex Kollars <alexkollars4@gmail.com>
2025-10-08 13:29:35 -0600

    Updated calulator

--- age_calculator.py
+++ age_calculator.py
@@ -2,12 +2,16 @@
 
 #Variable for user name input
 user_name = input('Enter your name: ')
+
+#variable for favorite hobby input
+favorite_hobby = input('Enter your favorite hobby: ')
+
 #Variable for user birth year input    
 birth_year = int(input('Enter your Birth Year: '))
+
 #Variable for current year input
-current_year = int(input('Enter CurrentYear: ' ))
+current_year = int(input('Enter Current Year: ' ))
 
-def  calculate_age(birth_year, current_year):
-    return current_year - birth_year
+current_age = current_year - birth_year
 
-print(f'Hello (user_name), you are years old.')
+print(f'Hello {user_name}, you are {current_age} years old and enjoy {favorite_hobby}.')
